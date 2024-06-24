88a376aba53a59cd914a13458e52e47424c83a21
Maksim Sholeninov <maksym.sholeninov@gmail.com>
2024-06-24 20:48:09 +0200

    обавил все файлы сайта по музыке

---
+++ header.php
@@ -0,0 +1,18 @@
+<header>
+    <div class="music-today">
+        <div class="text-quality-music">Music Today
+        </div>                
+        <div class="text-quality-music2">Are different
+        </div>  
+    </div> 
+    <div class="menu">
+            <div class="buttons-quality">Главная
+            </div>
+            <div class="buttons-quality news-button">Новости
+            </div>
+            <div class="buttons-quality">    
+                <a href="file:///C:/programming/music.loc/content.html">О нас
+                </a>
+            </div>
+        </div>
+</header>  
